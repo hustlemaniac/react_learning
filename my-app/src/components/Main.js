abab6e125ebf5ac29683dbfb71a265e35f35f85f
hustlemaniac <66681363+hustlemaniac@users.noreply.github.com>
2022-12-23 21:35:22 +0530

    added logo and end result

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+// import backlogo from './images/react-icon-large.png';
 
 export default function Main() {
     return (
         <main>
+            {/* <img src={backlogo} alt="Logo" className='main--icon'/> */}
             <h1 className='main--title'>Fun facts about React</h1>
             <ul className='main--facts'>
                 <li>Was first released in 2013</li>
