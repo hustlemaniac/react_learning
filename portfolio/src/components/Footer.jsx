9e0ba13c099158ea04418bbf70ff7be08483bd20
hustlemaniac <66681363+hustlemaniac@users.noreply.github.com>
2023-01-05 00:59:36 +0530

    added elements to footer section

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
-// import backlogo from './images/react-icon-large.png';
 
 export default function Footer() {
     return (
         <footer>
-            Socials go here
+            <div className='socials'>
+                    <a href="https://github.com/hustlemaniac?tab=repositories" target="_blank" class="github--icon"></a>
+                    <a href="https://www.instagram.com/abhenita/" target="_blank" class="instagram--icon"></a>
+            </div>
+            <div className='Copyright'>
+                <p>Copyright &copy; 2023 developed and maintained by Kamasamudram Bhavya Sai</p>
+            </div>
         </footer>
 
     )
