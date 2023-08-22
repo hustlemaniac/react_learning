29305dbfd79aa5ae77ddb4bbc59aec075a852485
hustlemaniac <bhavyasaikamasamudram@gmail.com>
2023-08-22 23:15:03 +0530

    new tutorials

---
+++ airbnb-project/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import grid from './images/photo-grid.png';
+
+export default function Navbar() {
+    return (
+        <section className="hero">
+            <img src={grid} alt="grid" className='section--grid'/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.
+            </p>
+        </section>
+
+    )
+}
---
+++ airbnb-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import logo from './images/airbnb-logo.png';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={logo} alt="logo" className='navbar--logo'/>
+        </nav>
+    )
+}
